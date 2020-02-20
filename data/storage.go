7b7aae8e7ebe499442696f63fc9ffc29219e0a4f
ronaldpayfazz <angry.ronald@payfazz.com>
2020-02-21 02:06:46 +0700

    remove table name in err not found

--- data/storage.go
+++ data/storage.go
@@ -112,7 +112,7 @@ func (r *PostgresStorage) Single(ctx *context.Context, elem interface{}, where s
 	err = statement.Get(elem, arg)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			ErrNotFound = fmt.Errorf("data is not found in table %s", r.tableName)
+			ErrNotFound = fmt.Errorf("data is not found")
 			return ErrNotFound
 		}
 		return err
@@ -148,7 +148,7 @@ func (r *PostgresStorage) SinglePOSTEMP(ctx *context.Context, elem interface{}, 
 	err = statement.Get(elem, arg)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			ErrNotFound = fmt.Errorf("data is not found in table %s", r.tableName)
+			ErrNotFound = fmt.Errorf("data is not found")
 			return ErrNotFound
 		}
 		return err
@@ -1121,7 +1121,7 @@ func (r *PostgresStorage) CountAll(ctx *context.Context, count interface{}) erro
 	err := db.Get(count, q)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			ErrNotFound = fmt.Errorf("data is not found in table %s", r.tableName)
+			ErrNotFound = fmt.Errorf("data is not found")
 			return ErrNotFound
 		}
 		return err
